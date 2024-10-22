a8dff091058a4c9894da5ff07ef21b6ab1926a4a
Jeffrey <jeffrey@1nteger.nl>
2024-10-22 11:23:17 +0200

    feat: unit demo for worker/manager comms

--- packages/worker/index.ts
+++ packages/worker/index.ts
@@ -1,27 +1,26 @@
 // workerNode.ts (Browser)
 import { createLibp2p, type Libp2p } from 'libp2p';
-import { webTransport } from '@libp2p/webtransport'
+import { webSockets } from '@libp2p/websockets'
 import { multiaddr } from '@multiformats/multiaddr';
 import { noise } from '@chainsafe/libp2p-noise'
-import { yamux } from '@chainsafe/libp2p-yamux'
+import {yamux} from '@chainsafe/libp2p-yamux'
 
 export async function createWorkerNode(): Promise<Libp2p> {
   const workerNode = await createLibp2p({
     transports: [
-      webTransport()
+      webSockets({
+      })
     ],
     addresses: {
-      listen: ['/dns4/localhost/tcp/15002/ws']
+      listen: ['/dns4/localhost/tcp/15001/ws'] // Worker listens here
     },
     connectionEncrypters: [
       noise()
     ],
-    streamMuxers: [yamux()],
+    streamMuxers: [
+      yamux()
+    ],
   });
 
   return workerNode
-}
-
-export function getMultiAddr(peerId: string) {
-  return multiaddr(`/dns4/localhost/tcp/15002/ws/p2p/${peerId}`)
 }
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,11 +1,34 @@
 // sum.test.js
-import { expect, test } from 'vitest'
+import { expect, test, afterEach, beforeEach } from 'vitest'
 
+import type {Libp2p} from 'libp2p'
 import { createManagerNode } from 'packages/manager'
+import { createWorkerNode } from 'packages/worker'
+import { multiaddr } from '@multiformats/multiaddr'
 
-export function sum(a: number, b: number) {
-    return a + b
-}
+let manager: Libp2p, worker: Libp2p
 
-test('creates a manager node', async () => {
+beforeEach(async () => {
+    manager = await createManagerNode()
+    worker = await createWorkerNode()
+    await manager.start();
+    await worker.start();
+})
+
+afterEach(async () => {
+   await manager.stop()
+   await worker.stop()
+})
+
+
+test('dials the manager node', async () => {
+    const managerAddress = multiaddr('/dns4/localhost/tcp/15000/ws');
+    const connection = await worker.dial(managerAddress)
+
+    // send hello from worker to manager
+    const stream = await connection.newStream('/task-flow/1.0.0')
+    await stream.sink([Buffer.from('hello from workerrr')])
+
+    // close connection
+    await connection.close()
 })
