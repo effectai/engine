8f91fe12e8ca2e35d227110aa37aba10da77e243
Jesse Eisses <jesse@eisses.email>
2025-01-08 13:13:39 +0100

    docs: Add token pie chart

--- docs/components/TokenCharts.tsx
+++ docs/components/TokenCharts.tsx
@@ -1,31 +1,15 @@
 import React from 'react';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Colors } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend, Colors);
 
 export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['EFX Holders', 'NFX Holders', 'Staking Incentives', 'DAO Treasury', 'Liquidity & Parnterships', 'Development Fund', 'Migration Reserve'],
   datasets: [
     {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
+      label: 'Tokens',
+      data: [185509140, 60636287, 50000000, 49049383, 68437032, 100000000, 6369158],
       borderWidth: 1,
     },
   ],
