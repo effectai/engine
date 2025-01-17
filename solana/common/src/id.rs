f2bd2c9d3382208e2c3fc515470566de54f746a7
Jesse Eisses <jesse@eisses.email>
2025-01-17 10:09:40 +0100

    migration: Update admin key to multisig

--- solana/common/src/id.rs
+++ solana/common/src/id.rs
@@ -7,7 +7,7 @@ use anchor_lang::declare_id;
  mod authority {
      use super::*;
      #[cfg(feature = "mainnet")]
-     declare_id!("admnVasr46cumirLqEXb5JderZEv3BMMcFnZwyK4Lo3");
+     declare_id!("nXwHwpf23pp1GVE9AXV3KJTN4orAqWGFgwHQT8E7qEx");
      #[cfg(not(feature = "mainnet"))]
      declare_id!("authGiAp86YEPGjqpKNxAMHxqcgvjmBfQkqqvhf7yMV");
  }
