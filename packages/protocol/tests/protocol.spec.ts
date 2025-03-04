11f53d66cebf085661b3ede7175a6927a8ede167
Jeffrieh <jeffrey@codifex.nl>
2025-03-04 12:56:24 +0100

    replace paymentAccount

--- packages/protocol/tests/protocol.spec.ts
+++ packages/protocol/tests/protocol.spec.ts
@@ -84,7 +84,7 @@ describe("Libp2p", () => {
 			n: 1,
 			mint: "EFFECT1A1R3Dz8Hg4q5SXKjkiPc6KDRUWQ7Czjvy4H7E",
 			recipient: "jeffCRA2yFkRbuw99fBxXaqE5GN3DwjZtmjV18McEDf",
-			paymentAccount: "blabla",
+			paymentAccount: "3pJUvyH1t4dBTyz622epbKkVYELVFk9hB5kU6Kmp3ien",
 		});
 
 		const authority = manager.peerId.publicKey?.raw;
