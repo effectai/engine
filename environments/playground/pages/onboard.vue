41e3664f6680e29fd214c76470d7d42b1e4d1efa
Jeffrieh <jeffrey@codifex.nl>
2025-03-25 22:14:39 +0100

    update onboarding centering

--- environments/playground/pages/onboard.vue
+++ environments/playground/pages/onboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <UCard class="max-w-lg mt-5 p-6">
+  <UCard class="max-w-lg mt-5 p-6 mx-auto">
     <h2 class="text-2xl font-title">Welcome to the Effect Worker Dashboard</h2>
 
     <p class="mt-3">
@@ -8,7 +8,9 @@
 
     <ul class="list-disc pl-5 mt-2">
       <li>Use your own Solana wallet</li>
-      <li>Use and import a worker node wallet if you don’t have one yet</li>
+      <li>
+        Use and import a worker node wallet if you don’t have one yet (WIP)
+      </li>
     </ul>
 
     <div class="flex items-center gap-3 mt-5">
