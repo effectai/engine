a75b9d2534a5e34ddd8a051e240ef7776cf6ef42
Jeffrieh <jeffrey@codifex.nl>
2025-04-08 17:56:51 +0200

    remove worker old

--- packages/protocol/src/worker-old/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./worker.js";
-export * from "./create.js";
-export * from "./consts.js";
-export { WorkerSession } from "./modules/session/service.js";
-export { WorkerTask } from "./modules/task/pb/WorkerTask.js";
