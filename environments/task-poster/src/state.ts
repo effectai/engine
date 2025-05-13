58575d84f4dc5f444efa6dc93f7660237022780e
Jesse Eisses <jesse@eisses.email>
2025-05-13 15:53:42 +0200

    Improve task-poster import logic and track finished datasets

--- environments/task-poster/src/state.ts
+++ environments/task-poster/src/state.ts
@@ -1,7 +1,7 @@
 import { KV } from "@cross/kv";
 export const managerId = "12D3KooWAb9rbnCHB9cgNgCcbrmtj73KKBrw5GkNgAC4SQRf9cPb";
 
-export const db = new KV();
+export const db = new KV({autoSync: true});
 
 // state to track the publish proress of CSV files
 export const publishProgress: Record<number, any> = {};
