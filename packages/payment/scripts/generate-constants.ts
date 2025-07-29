6ab260f71679a497625952329ae056570e69d25f
Jeffrieh <jeffrey@codifex.nl>
2025-07-29 15:22:17 +0200

    update build for payments

--- packages/payment/scripts/generate-constants.ts
+++ packages/payment/scripts/generate-constants.ts
@@ -1,7 +1,10 @@
 import fs from "node:fs";
 import dotenv from "dotenv";
+import path from "node:path";
 
-dotenv.config();
+const envFile =
+  process.env.NODE_ENV === "production" ? ".env.production" : ".env";
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
 const BATCH_SIZE = process.env.PAYMENT_BATCH_SIZE || 10;
 
