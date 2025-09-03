ccc67b7eb7b50aca79f069373f1a29060a51b64c
Jesse Eisses <jesse@eisses.email>
2025-09-03 11:49:50 +0200

    poster: Fix formatting

--- apps/task-poster/src/server.ts
+++ apps/task-poster/src/server.ts
@@ -1,25 +1,18 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import type { Express, Request, Response, NextFunction } from 'express';
 import express from "express";
 import { addAuthRoutes } from "./auth.js";
 import {
   addDatasetRoutes,
-  datasetIndex,
   getActiveDatasets,
-  getDatasets,
   startAutoImport,
 } from "./dataset.js";
 import type { DatasetRecord } from "./dataset.js";
 import * as dataset from "./dataset.js";
-import * as fetcher from "./fetcher.js";
-import { isHtmx, page } from "./html.js";
+import { page } from "./html.js";
 import { addLiveReload } from "./livereload.js";
 import * as state from "./state.js";
 import {
-  type TemplateRecord,
   addTemplateRoutes,
-  getTemplates,
 } from "./templates.js";
 
 const formatDate = (ts: number) =>
@@ -39,7 +32,7 @@ const campaignCard = (d: DatasetRecord) => {
 };
 
 const addMainRoutes = (app: Express) => {
-  app.get("/", async (req, res) => {
+  app.get("/", async (_: Request, res: Response) => {
     const datasets = await getActiveDatasets("active");
 
     const dsList = datasets.map((d) => campaignCard(d));
@@ -47,17 +40,17 @@ const addMainRoutes = (app: Express) => {
     const oldDs = (await getActiveDatasets("finished"))
       .concat(await getActiveDatasets("archived"))
       .map(
-        (d) => `
+	(d) => `
 <a href="/d/${d.id}">${d.name}</a> <small>${d.id}</small>`,
       );
 
     res.send(
       page(
-        `
+	`
 <section>
   <h2>Active Campaigns (${dsList.length})</h2>
   <p>The following datasets are currently being crafted by people and AI around the globe, ` +
-          `brought to you by Effect AI.</p>
+	  `brought to you by Effect AI.</p>
 
   <div class="boxbox">
   ${
@@ -98,7 +91,7 @@ const main = async () => {
   app.use(express.json({ limit: "2mb" }));
 
   // gracefull error when files are too lar1ge
-  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
     if ((err as any).status === 413) {
       // TODO: use htmx-ext-response-targets for a 413
       res.setHeader("HX-Retarget", "#messages");
