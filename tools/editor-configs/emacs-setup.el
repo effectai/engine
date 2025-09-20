9ba49132c5392b5cb62dac61d949382160eb0ba0
Jesse Eisses <jesse@eisses.email>
2025-09-20 15:50:23 +0200

    Add emacs helper functions to setup a dev environment

---
+++ tools/editor-configs/emacs-setup.el
@@ -0,0 +1,44 @@
+;;; Emacs developer functions for Effect Mono Repo
+
+;;; Commentary:
+;; This file provides functions to help work in the Effect monorepo
+;;
+;; 1. Load this file: (load "tools/editor-configs/emacs-setup.el")
+;;
+;; This depends on the vterm library
+;;
+;; Functions provided:
+;;
+;; - effect/setup-terminals: opens dedicated vterms for: manager and
+;; task poster, each in a guix container
+
+(defun effect/open-project-terminals (root)
+  (delete-other-windows)
+  (let ((default-directory root))
+    (vterm (format "*vterm- run manager"))
+    (vterm-send-string "guix shell -m tools/guix/manifest.scm -L tools/guix/extra  --container --network --emulate-fhs python jq curl")
+    (vterm-send-return)
+    (vterm-send-string "node --watch tools/cli/dist/index.js manager start -k tools/keys/authGiAp86YEPGjqpKNxAMHxqcgvjmBfQkqqvhf7yMV.json")
+    (vterm-send-return)
+
+    (split-window-below)
+    (vterm (format "*vterm- run poster"))
+    (vterm-send-string "guix shell -m tools/guix/manifest.scm -L tools/guix/extra  --container --network --emulate-fhs python jq curl")
+    (vterm-send-return)
+    (vterm-send-string "cd apps/task-poster")
+    (vterm-send-return)
+    (vterm-send-string "pnpm dev")
+    (vterm-send-return)
+
+    (balance-windows)))
+
+(defun effect/setup-terminals ()
+  "Open several vterms for Effect dev."
+  (interactive)
+  (if-let ((root (project-current)))
+      (let ((root-dir (project-root root)))
+	(message "Opening terminal in %S" root-dir)
+	(my/open-effect-terminals root-dir))
+    (user-error "Not in a project")))
+
+(provide 'effect/emacs-setup)
