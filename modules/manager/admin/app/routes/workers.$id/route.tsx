a473a0a2b33eda2355e3d6a090d527889589d25d
Miguel Nobre <mnobre24@gmail.com>
2025-10-03 14:36:38 -0400

    added unban to admin dashboard

--- modules/manager/admin/app/routes/workers.$id/route.tsx
+++ modules/manager/admin/app/routes/workers.$id/route.tsx
@@ -33,6 +33,9 @@ export default function Component() {
           <Button type="submit" name="intent" value="ban">
             Ban
           </Button>
+          <Button type="submit" name="intent" value="unban">
+            Unban
+          </Button>
           <Button type="submit" name="intent" value="revoke">
             Revoke Access Code
           </Button>
@@ -66,6 +69,11 @@ export const action = async ({
   } else if (intent === "ban") {
     await context.workerManager.updateWorkerState(id, (state) => ({
       banned: true,
+    }));
+    context.workerManager.workerQueue.removePeer(id);
+  } else if (intent === "unban") {
+    await context.workerManager.updateWorkerState(id, (state) => ({
+      banned: false,
     }));
     context.workerManager.workerQueue.removePeer(id);
   } else if (intent === "revoke") {
